38817ba9b88d6edb534af26ac84aa30fa666fd8d
PikawaiMoon <j.clein@escola.pr.gov.br>
2026-03-05 12:47:18 +0000

    entendendo variaveis em python

---
+++ aulas/aula02.py
@@ -0,0 +1,8 @@
+# Dados numéricos 
+umidade do ar = 80
+temperatura = 26 
+velocidade do vento = 20 
+
+# dados categoricos 
+
+cidades = ['Verê', 'Francisco Beltrao', 'Dois vizinhos', 'Itapejara', 'São João',]
