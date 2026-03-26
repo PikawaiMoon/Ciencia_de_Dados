b02ea929c365dca4fa2c8c91cd6d3fee1062cec0
PikawaiMoon <j.clein@escola.pr.gov.br>
2026-03-26 12:11:43 +0000

    Data Vizualization.py

---
+++ aulas/aula03.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
+vendas = [200, 300, 340, 305, 360, 400]
+
+plt.plot(meses, vendas, color= 'red' )
+plt.title('Vendas no Primeiro Semestre')
+
+plt.xlabel('Meses')
+plt.ylabel('Vendas')
+
+plt.show()
+plt.savefig('aula03.png')
